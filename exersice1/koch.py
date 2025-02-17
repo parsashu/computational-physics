c34538faed61e86f05700a9014a2ba87e41afdcb
parsashu <a.zshahidi91@gmail.com>
2025-02-17 14:55:30 +0330

    transform to matrix

--- exersice1/koch.py
+++ exersice1/koch.py
@@ -2,43 +2,33 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Define three endpoints
-x1, y1 = -10, 0  # First point
-x2, y2 = -5, 0  # Second point
-x3, y3 = 0, 10 * np.sqrt(3) / 2  # Second point
-x4, y4 = 5, 0  # Third point
-x5, y5 = 10, 0  # Fourth point
+start_point = np.array([-0.5, 0])
+end_point = np.array([0.5, 0])
+
 
 # Generate points between the endpoints for first line
 num_points = 100  # Number of points to create smooth lines
-t = np.linspace(0, 1, num_points)
+t = np.linspace(0, 1, num_points)[:, np.newaxis]
 
 # Parametric equations for first line
-x_line1 = x1 + (x2 - x1) * t
-y_line1 = y1 + (y2 - y1) * t
+line1 = start_point + (end_point - start_point) * t
 
-# Parametric equations for second line
-x_line2 = x2 + (x3 - x2) * t
-y_line2 = y2 + (y3 - y2) * t
 
-# Parametric equations for third line
-x_line3 = x3 + (x4 - x3) * t
-y_line3 = y3 + (y4 - y3) * t
+def find_on_line_dots(start_point, end_point):
+    pass
 
-# Parametric equations for fourth line
-x_line4 = x4 + (x5 - x4) * t
-y_line4 = y4 + (y5 - y4) * t
 
 # Create the plot
 plt.figure(figsize=(8, 4))
 
 # Plot both lines
-plt.plot(x_line1, y_line1, "b-")
-plt.plot(x_line2, y_line2, "b-")
-plt.plot(x_line3, y_line3, "b-")
-plt.plot(x_line4, y_line4, "b-")
+plt.plot(line1[:, 0], line1[:, 1], "b-")  # Plot x and y coordinates separately
 
 # Plot endpoints
-plt.plot([x1, x2, x3, x4, x5], [y1, y2, y3, y4, y5], "ro")  # Show endpoints as red dots
+plt.plot(
+    [start_point[0], end_point[0]], [start_point[1], end_point[1]], "ro"
+)  # Show endpoints as red dots
+
 
 plt.axis("equal")
 plt.grid(True)

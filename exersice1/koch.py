27c0c578776e7e3820fe2dc74ad6f4385a1c9438
parsashu <a.zshahidi91@gmail.com>
2025-02-18 11:11:31 +0330

    add scalable functions

--- exersice1/koch.py
+++ exersice1/koch.py
@@ -4,8 +4,11 @@ import matplotlib.pyplot as plt
 # Define three endpoints
 start_point = np.array([-0.5, 0])
 end_point = np.array([0.5, 0])
+all_points = [start_point, end_point]
+all_lines = []
 
 
+# Point finder functions
 def find_devider_points(start_point, end_point):
     direction = end_point - start_point
     line_length = np.linalg.norm(end_point - start_point)
@@ -16,17 +19,11 @@ def find_devider_points(start_point, end_point):
     return point1, point2
 
 
-point1, point2 = find_devider_points(start_point, end_point)
-
-
 def find_middle_point(start_point, end_point):
     direction = end_point - start_point
     line_length = np.linalg.norm(end_point - start_point)
     unit_vector = direction / line_length
     return start_point + (line_length / 2) * unit_vector
-
-
-middle_point = find_middle_point(start_point, end_point)
 
 
 def find_top_point(start_point, end_point):
@@ -41,46 +38,54 @@ def find_top_point(start_point, end_point):
     return middle_point + triangle_height * perpendicular
 
 
-top_point = find_top_point(start_point, end_point)
+def find_new_points(start_point, end_point):
+    point1, point2 = find_devider_points(start_point, end_point)
+    top_point = find_top_point(start_point, end_point)
+    return point1, point2, top_point
 
 
+def add_new_points(start_point, end_point):
+    point1, point2, top_point = find_new_points(start_point, end_point)
+    all_points.pop()
+    all_points.append(point1)
+    all_points.append(top_point)
+    all_points.append(point2)
+    all_points.append(end_point)
+
+
+# Line functions
 def line_between_points(start_point, end_point):
     num_points = 100  # Number of points to create smooth lines
     t = np.linspace(0, 1, num_points)[:, np.newaxis]
     return start_point + (end_point - start_point) * t
 
 
-line1 = line_between_points(start_point, point1)
-line2 = line_between_points(point1, top_point)
-line3 = line_between_points(top_point, point2)
-line4 = line_between_points(point2, end_point)
+def add_new_lines():
+    for i in range(len(all_points) - 1):
+        line = line_between_points(all_points[i], all_points[i + 1])
+        all_lines.append(line)
 
 
 # Create the plot
 plt.figure(figsize=(8, 4))
 
 
-# Plot lines
-def plot_line(line):
-    plt.plot(line[:, 0], line[:, 1], "b-")
-
-
-plot_line(line1)
-plot_line(line2)
-plot_line(line3)
-plot_line(line4)
+# Plot functions
+def plot_lines():
+    for line in all_lines:
+        plt.plot(line[:, 0], line[:, 1], "b-")
 
 
 # Plot points
-def plot_point(point):
-    plt.plot(point[0], point[1], "ro")
+def plot_points():
+    for point in all_points:
+        plt.plot(point[0], point[1], "ro")
 
 
-plot_point(start_point)
-plot_point(end_point)
-plot_point(point1)
-plot_point(point2)
-plot_point(top_point)
+add_new_points(start_point, end_point)
+add_new_lines()
+plot_points()
+plot_lines()
 
 
 plt.axis("equal")

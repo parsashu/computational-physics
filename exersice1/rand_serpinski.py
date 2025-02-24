1fbdce677d0f1854114b2859aa9e0b5ad4237143
parsashu <a.zshahidi91@gmail.com>
2025-02-24 10:25:37 +0330

    add random point generator

--- exersice1/rand_serpinski.py
+++ exersice1/rand_serpinski.py
@@ -1,14 +1,24 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-iterations = 16
+np.random.seed(20)
+
+iterations = 7
+
+# Create triangle points
 initial_points = np.array(
     [
-        [0.0, 0.0],
-        [0.5, 0.5],
-        [1.0, 0.0],
+        [0.0, 0.0],  # bottom left
+        [0.5, 0.866],  # top point (using sin(60°) = 0.866)
+        [1.0, 0.0],  # bottom right
     ]
 )
+
+A = initial_points[0]
+B = initial_points[1]
+C = initial_points[2]
+
+all_points = initial_points.copy()
 
 
 def scale_shape(points, scale_factor, center_point=None):
@@ -31,26 +41,57 @@ def scale_shape(points, scale_factor, center_point=None):
     return scaled + center_point
 
 
-def rotate_shape(points, angle_degrees, center_point=None):
-    """Rotate points around a center point"""
+def random_point_generator():
+    r1 = np.random.rand()
+    r2 = np.random.rand()
 
-    # If no center point given, use center of points
-    if center_point is None:
-        center_point = np.mean(points, axis=0)
+    # Reflect (r1, r2) inside the triangle if r1 + r2 > 1
+    if r1 + r2 > 1:
+        r1 = 1 - r1
+        r2 = 1 - r2
 
-    # Convert angle to radians
-    theta = np.radians(angle_degrees)
+    # (1-r1-r2)*A + r1*B + r2*C
+    point = (1 - r1 - r2) * A + r1 * B + r2 * C
+    return point
 
-    # Create rotation matrix
-    rotation_matrix = np.array(
-        [[np.cos(theta), -np.sin(theta)], [np.sin(theta), np.cos(theta)]]
-    )
 
-    # Translate to origin
-    translated = points - center_point
+# Generate a random point
+point = random_point_generator()
 
-    # Rotate points
-    rotated = np.dot(translated, rotation_matrix.T)
+# Create the plot
+plt.figure(figsize=(8, 6))
 
-    # Translate back
-    return rotated + center_point
+# Plot initial triangle points
+plt.scatter(
+    initial_points[:, 0],
+    initial_points[:, 1],
+    color="blue",
+    s=100,
+    label="Triangle vertices",
+)
+
+# Plot random point
+plt.scatter(point[0], point[1], color="red", s=100, label="Random point")
+
+# Plot triangle edges
+plt.plot(
+    [initial_points[0, 0], initial_points[1, 0]],
+    [initial_points[0, 1], initial_points[1, 1]],
+    "b-",
+)
+plt.plot(
+    [initial_points[1, 0], initial_points[2, 0]],
+    [initial_points[1, 1], initial_points[2, 1]],
+    "b-",
+)
+plt.plot(
+    [initial_points[2, 0], initial_points[0, 0]],
+    [initial_points[2, 1], initial_points[0, 1]],
+    "b-",
+)
+
+plt.legend()
+plt.axis("equal")
+plt.grid(True)
+plt.title("Triangle with Random Point")
+plt.show()

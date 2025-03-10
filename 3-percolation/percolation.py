39849fd87afaf5d454d13f0947140de96f2ece03
parsashu <a.zshahidi91@gmail.com>
2025-03-10 13:32:17 +0330

    add turn_on

--- 3-percolation/percolation.py
+++ 3-percolation/percolation.py
@@ -1,25 +1,30 @@
 import numpy as np
 import matplotlib.pyplot as plt
+
 L = 10
 
 grid = np.zeros((L, L))
-grid[0, :] = 1
+
+
+def turn_on(grid, p):
+    """Open sites with probability p"""
+    random_values = np.random.random((L, L))
+    grid = np.where(random_values < p, 1, 0)
+    return grid
+
+
+gird_on = turn_on(grid, 0.5)
+
 
 plt.figure(figsize=(6, 6))
-colors = ['red', 'blue']
-colors_with_alpha = [(1, 0, 0, 0.7), (0, 0, 1, 0.7)]  
+colors = ["purple", "blue"]
+colors_with_alpha = [(0.8, 0, 0.8, 0.7), (0, 0, 1, 0.7)]
 cmap = plt.matplotlib.colors.ListedColormap(colors_with_alpha)
-plt.imshow(grid, cmap=cmap, vmin=0, vmax=1)
+plt.imshow(gird_on, cmap=cmap, vmin=0, vmax=1)
 plt.clim(-0.5, 1.5)
-plt.grid(False)  
+plt.grid(False)
 ax = plt.gca()
-ax.set_xticks(np.arange(-.5, L, 1), minor=True)
-ax.set_yticks(np.arange(-.5, L, 1), minor=True)
-ax.grid(which='minor', color='white', linestyle='-', linewidth=0.5)
+ax.set_xticks(np.arange(-0.5, L, 1), minor=True)
+ax.set_yticks(np.arange(-0.5, L, 1), minor=True)
+ax.grid(which="minor", color="white", linestyle="-", linewidth=0.5)
 plt.show()
-
-
-
-
-
-

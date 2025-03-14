2868fbcfe619bbc5363689a5d390df865fb061a2
parsashu <a.zshahidi91@gmail.com>
2025-03-14 09:48:02 +0330

    finish algorithm

--- 3-percolation/hoshen_kopelman.py
+++ 3-percolation/hoshen_kopelman.py
@@ -6,7 +6,8 @@ length = 10
 p = 0.5
 k = 2
 L = {}
-# np.random.seed(12)
+S = {1: length}
+np.random.seed(12)
 
 grid = np.zeros((length, length))
 grid[:, 0] = 1
@@ -15,46 +16,46 @@ random_values = np.random.random((length, length))
 
 
 def get_up_left_neighbors(grid, i, j):
-    neighbors_coords = []
-    neighbors_colors = []
+    neighbors_k = []
 
     # Up
     if i > 0 and grid[i - 1, j] != 0:
-        neighbors_coords.append((i - 1, j))
-        neighbors_colors.append(grid[i - 1, j])
+        neighbors_k.append(grid[i - 1, j])
 
     # Left
     if j > 0 and grid[i, j - 1] != 0:
-        neighbors_coords.append((i, j - 1))
-        neighbors_colors.append(grid[i, j - 1])
+        neighbors_k.append(grid[i, j - 1])
 
-    return neighbors_coords, neighbors_colors
+    return neighbors_k
 
 
 for j in range(length):
     for i in range(length):
         if random_values[i, j] < p and grid[i, j] == 0:
-            neighbors_coords, neighbors_colors = get_up_left_neighbors(grid, i, j)
+            neighbors_k = get_up_left_neighbors(grid, i, j)
 
-            if len(neighbors_coords) == 0:
+            if len(neighbors_k) == 0:
                 grid[i, j] = k
                 L[k] = k
+                S[k] = 1              
                 k += 1
 
-            elif len(neighbors_coords) == 1:
-                grid[i, j] = neighbors_colors[0]
+            elif len(neighbors_k) == 1:
+                k = int(neighbors_k[0])
+                grid[i, j] = k
+                S[k] += 1
 
             else:
-                k_up = neighbors_colors[0]
-                k_left = neighbors_colors[1]
+                k_up = int(neighbors_k[0])
+                k_left = int(neighbors_k[1])
                 grid[i, j] = k_left
                 L[k_up] = k_left
+                S[k_left] += S[k_up] + 1
 
 
 plt.figure(figsize=(6, 6))
 colors = plt.cm.rainbow(np.linspace(0, 1, int(k)))
 colors[0] = (0.3, 0.3, 0.3, 1.0)
-colors[-1] = (0, 0, 0, 1.0)
 colors[1] = (0, 0, 0, 0)
 cmap = ListedColormap(colors)
 

1f6fb05eb8114ce4e1f666371b75f0afa0f873eb
parsashu <a.zshahidi91@gmail.com>
2025-03-15 12:39:09 +0330

    finish plotting and add is_percolate

--- 3-percolation/hoshen_kopelman.py
+++ 3-percolation/hoshen_kopelman.py
@@ -2,8 +2,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.colors import ListedColormap
 
-length = 10
-p = 0.5
+length = 50
+p = 0.6
 k = 2
 L = {}
 S = {1: length}
@@ -37,21 +37,53 @@ for j in range(length):
             if len(neighbors_k) == 0:
                 grid[i, j] = k
                 L[k] = k
-                S[k] = 1              
+                S[k] = 1
                 k += 1
 
             elif len(neighbors_k) == 1:
-                k = int(neighbors_k[0])
-                grid[i, j] = k
-                S[k] += 1
+                k_neighbor = int(neighbors_k[0])
+                grid[i, j] = k_neighbor
+                S[k_neighbor] += 1
 
             else:
                 k_up = int(neighbors_k[0])
                 k_left = int(neighbors_k[1])
-                grid[i, j] = k_left
-                L[k_up] = k_left
-                S[k_left] += S[k_up] + 1
+                if k_up != k_left:
+                    grid[i, j] = k_left
+                    L[k_up] = k_left
+                    S[k_left] += S[k_up] + 1
+                    S[k_up] = 0
+                else:
+                    grid[i, j] = k_left
+                    S[k_left] += 1
 
+
+def root(label):
+    if label not in L:
+        L[label] = label
+
+    while label != L[label]:
+        label = L[label]
+    return label
+
+
+for i in range(length):
+    for j in range(length):
+        if grid[i, j] > 0:
+            grid[i, j] = root(int(grid[i, j]))
+
+
+def is_percolating():
+    left_root = root(1)
+
+    for cell in grid[:, -1]:
+        if cell != 0:
+            if L[int(cell)] == left_root:
+                return True
+    return False
+
+
+percolates = is_percolating()
 
 plt.figure(figsize=(6, 6))
 colors = plt.cm.rainbow(np.linspace(0, 1, int(k)))
@@ -68,5 +100,5 @@ ax.set_yticks(np.arange(-0.5, length, 1), minor=True)
 ax.grid(which="minor", color="white", linestyle="-", linewidth=0.1)
 plt.xticks([])
 plt.yticks([])
-plt.title(f"Percolation Clusters (L={length}, p={p})")
+plt.title(f"Percolation Clusters (L={length}, p={p}, Percolates={percolates})")
 plt.show()

e37d01654f55b718e21e579757cfe1521f220120
parsashu <a.zshahidi91@gmail.com>
2025-03-22 12:24:28 +0330

    add variance

--- 4-random walk/random_walk.py
+++ 4-random walk/random_walk.py
@@ -27,38 +27,45 @@ def single_random_walk(num_steps):
     return path
 
 
-def ensemble_average(num_steps, num_trials):
+def ensemble_average(num_steps, ensamble):
     """Calculates the ensemble average over multiple trials."""
-    all_paths = np.zeros((num_trials, num_steps + 1))
+    all_paths = np.zeros((ensamble, num_steps + 1))
 
-    for trial in range(num_trials):
+    for trial in range(ensamble):
         all_paths[trial, :] = single_random_walk(num_steps)
 
     mean_positions = np.mean(all_paths, axis=0)
-    return mean_positions
+    mean_positions_square = np.mean(all_paths**2, axis=0)
+    variance = mean_positions_square - mean_positions**2
+    return mean_positions, variance
 
 
-def theoretical_mean(times):
+def theorical_mean(times):
     return (l / T) * (p - q) * np.array(times)
 
 
+def theoretical_variance(times):
+    return 4 * l**2 * (p * q) * (1 / T) * np.array(times)
+
+
 times = [step * T for step in range(num_steps + 1)]
-empirical_mean = ensemble_average(num_steps, ensamble)
-theoretical = theoretical_mean(times)
+sim_mean, sim_variance = ensemble_average(num_steps, ensamble)
+theo_mean = theorical_mean(times)
+theo_variance = theoretical_variance(times)
 single_path = single_random_walk(num_steps)
 
 plt.figure(figsize=(10, 6))
 plt.plot(times, single_path, alpha=0.5, label="Example Random Walk")
 plt.plot(
     times,
-    empirical_mean,
+    sim_mean,
     "g-",
     linewidth=2,
-    label=f"Empirical Mean ({ensamble} trials)",
+    label=f"Simulated Mean (ensamble = {ensamble})",
 )
 plt.plot(
     times,
-    theoretical,
+    theo_mean,
     "r--",
     linewidth=2,
     label="Theoretical Mean: $\\langle x(t) \\rangle = \\frac{l}{\\tau}(p-q)t$",
@@ -66,6 +73,30 @@ plt.plot(
 plt.title(f"One-dimensional Random Walk\np={p}, l={l}, T={T}, N={num_steps}")
 plt.xlabel("Time")
 plt.ylabel("Position")
+plt.grid(True)
+plt.legend()
+plt.show()
+
+plt.figure(figsize=(10, 6))
+plt.plot(
+    times,
+    sim_variance,
+    "g-",
+    linewidth=2,
+    label=f"Simulated Variance (ensamble = {ensamble})",
+)
+plt.plot(
+    times,
+    theo_variance,
+    "r--",
+    linewidth=2,
+    label="Theoretical Variance: $\\sigma^2(t) = 4l^2pq\\frac{t}{\\tau}$",
+)
+plt.title(
+    f"Variance of One-dimensional Random Walk\np={p}, l={l}, T={T}, N={num_steps}"
+)
+plt.xlabel("Time")
+plt.ylabel("Variance")
 plt.grid(True)
 plt.legend()
 plt.show()

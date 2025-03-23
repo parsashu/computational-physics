ec0e7f2649ffcd5b824eeeb26e84cd1cc5e41886
parsashu <a.zshahidi91@gmail.com>
2025-03-23 12:19:51 +0330

    finish life time

--- 4-random walk/random_walk_trap.py
+++ 4-random walk/random_walk_trap.py
@@ -5,14 +5,16 @@ p = 0.5
 l = 1
 T = 1
 q = 1 - p
+x0 = -10
+delta_x = 1
 trap_x = 10
-ensamble = 500
+ensamble = 10000
 
 
-def single_random_walk():
+def single_random_walk(x0):
     """Simulates a single random walk."""
-    position = 0
-    path = [0]
+    position = x0
+    path = [x0]
 
     while position <= trap_x and position >= -trap_x:
         step_direction = np.random.choice([-l, l], p=[q, p])
@@ -23,17 +25,17 @@ def single_random_walk():
     return path, times
 
 
-def ensemble_average(num_steps, ensamble):
-    """Calculates the ensemble average over multiple trials."""
-    all_paths = np.zeros((ensamble, num_steps + 1))
+def mean_life_time(ensamble, x0):
+    """Calculates the mean life time over multiple trials."""
+    life_times = []
 
-    for trial in range(ensamble):
-        all_paths[trial, :] = single_random_walk(num_steps)
+    for _ in range(ensamble):
+        _, times = single_random_walk(x0)
+        life_time = times[-1]
+        life_times.append(life_time)
 
-    mean_positions = np.mean(all_paths, axis=0)
-    mean_positions_square = np.mean(all_paths**2, axis=0)
-    variance = mean_positions_square - mean_positions**2
-    return mean_positions, variance
+    mean_life_time = np.mean(life_times)
+    return mean_life_time
 
 
 def theorical_mean(times):
@@ -44,15 +46,23 @@ def theoretical_variance(times):
     return 4 * l**2 * (p * q) * (1 / T) * np.array(times)
 
 
-single_path, times = single_random_walk()
+mean_life_times_list = []
+x0_list = []
 
+while x0 <= trap_x:
+    mean_life_time_ = mean_life_time(ensamble, x0)
+
+    mean_life_times_list.append(mean_life_time_)
+    x0_list.append(x0)
+
+    x0 += delta_x
 
 plt.figure(figsize=(10, 6))
-plt.plot(times, single_path, alpha=0.5, label="Example Random Walk")
-plt.title(f"One-dimensional Random Walk\np={p}, l={l}, T={T}")
-plt.xlabel("Time")
-plt.ylabel("Position")
+plt.plot(x0_list, mean_life_times_list, "o")
+plt.title(
+    f"Mean Life Time vs Initial Position\np={p}"
+)
+plt.xlabel("Initial Position (x₀)")
+plt.ylabel("Mean Life Time")
 plt.grid(True)
-plt.legend()
-plt.xticks(np.linspace(min(times), max(times), 10))
 plt.show()

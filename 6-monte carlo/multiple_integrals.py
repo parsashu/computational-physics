65f2c42330349daefac486f25d95d5b1f258f74f
parsashu <a.zshahidi91@gmail.com>
2025-04-10 22:31:50 +0330

    finished Zcm

--- 6-monte carlo/multiple_integrals.py
+++ 6-monte carlo/multiple_integrals.py
@@ -2,44 +2,54 @@ import numpy as np
 
 
 def rho(r, theta):
-    # return 0.25 * r * np.cos(theta) + 0.75
-    return 1
+    return 0.25 * r * np.cos(theta) + 0.75
 
 
 def dm(r, theta):
     return r**2 * np.sin(theta) * rho(r, theta)
 
 
-def r2_dm(r, theta):
-    return r**2 * dm(r, theta)
+def z_dm(r, theta):
+    return r * np.cos(theta) * dm(r, theta)
 
 
 def random_coordinates():
     r = np.random.uniform(0, 1)
     theta = np.random.uniform(0, np.pi)
-    phi = np.random.uniform(0, 2 * np.pi)
-    return r, theta, phi
+    return r, theta
 
 
-def monte_carlo(N):
-
+def Z_cm(N):
     mean_m = 0
+    mean_z = 0
     mean_sqr_m = 0
+    mean_sqr_z = 0
 
     for _ in range(N):
-        r, theta, phi = random_coordinates()
+        r, theta = random_coordinates()
 
         mean_m += dm(r, theta)
+        mean_z += z_dm(r, theta)
         mean_sqr_m += (dm(r, theta)) ** 2
+        mean_sqr_z += (z_dm(r, theta)) ** 2
 
     mean_m /= N
+    mean_z /= N
     mean_sqr_m /= N
+    mean_sqr_z /= N
 
-    I = mean_m * 2 * np.pi**2
-    sigma = np.sqrt(mean_sqr_m - mean_m**2)
-    delta = sigma / np.sqrt(N)
+    M = mean_m * 2 * np.pi**2
+    sigma_m = np.sqrt(mean_sqr_m - mean_m**2)
 
-    return float(I), float(sigma), float(delta)
+    Z = mean_z * 2 * np.pi**2
+    sigma_z = np.sqrt(mean_sqr_z - mean_z**2)
+
+    Z_cm = Z / M
+    sigma_Z_cm = abs(Z_cm) * np.sqrt((sigma_z / Z) ** 2 + (sigma_m / M) ** 2)
+    delta_Z_cm = sigma_Z_cm / np.sqrt(N)
+
+    return float(Z_cm), float(delta_Z_cm)
 
 
-print(monte_carlo(1000000)[0] / np.pi)
+Z_cm_value, Z_cm_error = Z_cm(1000000)
+print(f"Z_cm = {Z_cm_value:.6f} ± {Z_cm_error:.6f}")

bc48029b7750ae2e67f07c3afd38d59d54ea6298
parsashu <a.zshahidi91@gmail.com>
2025-05-11 10:57:24 +0330

    cahnged V to Q

--- 9-ODE/RC.py
+++ 9-ODE/RC.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 
 R = 1e3
 C = 1e-6
-V0 = 5
+Q0 = 5
 tau = R * C
 
 T = 0.01
@@ -11,17 +11,17 @@ dt = 0.0001
 n_steps = int(T / dt)
 
 t = np.linspace(0, T, n_steps)
-V = np.zeros(n_steps)
-V[0] = V0
+Q = np.zeros(n_steps)
+Q[0] = Q0
 
 for i in range(n_steps - 1):
-    dV = -V[i] / tau * dt
-    V[i + 1] = V[i] + dV
+    dQ = -Q[i] / tau * dt
+    Q[i + 1] = Q[i] + dQ
 
-plt.plot(t, V, label="RC Voltage")
+plt.plot(t, Q, label="RC Charge")
 plt.xlabel("Time (s)")
-plt.ylabel("Voltage (V)")
-plt.title(f"RC Voltage (dt = {dt}s)")
+plt.ylabel("Charge (C)")
+plt.title(f"RC Charge (dt = {dt}s)")
 plt.legend()
 plt.grid(True)
 plt.show()

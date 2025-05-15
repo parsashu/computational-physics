fbf4501d4810657a55fd1d6b9c053327ced95d5d
parsashu <a.zshahidi91@gmail.com>
2025-05-15 13:46:24 +0330

    added alg instability

--- 10-algorithm & chaos/1-alg_instability.py
+++ 10-algorithm & chaos/1-alg_instability.py
@@ -11,16 +11,18 @@ n_steps = int(T / h)
 
 t = np.linspace(0, T, n_steps)
 Q = np.zeros(n_steps)
-Q[0] = 5
 
-for i in range(n_steps - 1):
-    dQ = -Q[i] / tau * h
-    Q[i + 1] = Q[i] + dQ
+Q[0] = 5
+Q[1] = 5 * (1 - h / tau)
+
+for n in range(1, n_steps - 1):
+    f_n = -Q[n] / tau
+    Q[n + 1] = Q[n - 1] + 2 * f_n * h
 
 plt.plot(t, Q, label="RC Charge")
 plt.xlabel("Time (s)")
 plt.ylabel("Charge (C)")
-plt.title(f"RC Charge (h = {h}s)")
+plt.title(f"RC Charge using new algorithm (h = {h}s)")
 plt.legend()
 plt.grid(True)
 plt.show()
